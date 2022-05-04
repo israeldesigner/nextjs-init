243ab595b5b7b22400d79ef6c8bac9d6d61ef955
Israel <israeldesigner@live.estacio.br>
2022-05-04 00:15:24 -0300

    stylus header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Acessibility from '../acessibility';
 import Image from 'next/image';
 import LogoBranca from '../../public/logo-branca.png'
@@ -8,74 +8,37 @@ const Header: React.FunctionComponent = (props) => {
     return(
       <header id="header" className="">
         <Acessibility />
-        <div id="contHeader" className="container-fluid">
-            <nav className="h-nav navbar navbar-expand-lg">
-                <div className="h-nav__links--order">
-                    <a href="https://www.fortaleza.ce.gov.br/" target="_blank" className="h-nav__mainBrand" rel="noreferrer">
-                        <Image src={LogoBranca}  width={185} height={185}  alt="Logo da Prefeitura Branca"  id="logoPrefBranca" className="h-nav__brand--hidden"/>
-                    </a>
-                    <ul className="h-nav__menu--list navbar-nav ml-auto">
-                        <li className="nav-item">
-                        <a className="nav-link" href="/">Início</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link linkAnchorCom" href="#">Sobre</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorRen" href="#">Como se conectar</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorFor" href="#">Pontos de Wi-Fi</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorPar" href="#">Parceiros</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorFaq" href="#">Dúvidas</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorCon" href="#">Fale conosco</a>
-                        </li>
-                    </ul>
-                    <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#mobileMenu" aria-controls="mobileMenu"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"><i className="icon-menu icons text-white"></i></span>
-                    </button>
-                </div>
-
-                <div className="h-nav_menu--hidden collapse navbar-collapse bg-white" id="mobileMenu">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorAl" href="/">Início</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorCom" href="#">Sobre</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkCursos" href="#">Como se conectar</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorFor" href="#">Pontos de Wi-fi</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link linkAnchorPar" href="#">Parceiros</a>
-                        </li>
-                        <li className="nav-item ">
-                            <a className="nav-link linkAnchorFaq" href="#">Dúvidas</a>
-                        </li>
-                        <li className="nav-item ">
-                            <a className="nav-link linkAnchorCon" href="#">Fale conosco</a>
-                        </li>
-
-                        <li className="nav-item mt-2">
-                            <button type="button"  className="btn btn-warning btn-block nav-link ">
-                                <i className="icon-login icons mr-1"></i>Cadastrar
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <Navbar 
+            bg="success" 
+            expand="lg"
+            sticky="top" 
+            collapseOnSelect
+        >
+            <Container id="contHeader">
+                <Navbar.Brand href="#home" className='navbar' style={{ width: "185px", height: "58px", position: "relative" }}>
+                    <Image 
+                        src={LogoBranca}  
+                        alt="Logo da Prefeitura Branca"  
+                        id="logoPrefBranca" 
+                        className=""
+                        layout="fill"
+                        priority={true}
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="h-nav justify-content-end">
+                    <Nav className="h-nav__menu--list navbar-nav">
+                        <Nav.Link href="#home" className="nav-link">Início</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link">Sobre</Nav.Link>
+                        <Nav.Link href="#home" className="nav-link">Como se conectar</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link">Pontos de Wi-Fi</Nav.Link>
+                        <Nav.Link href="#home" className="nav-link">Parceiros</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link">Dúvidas</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link">Fale conosco</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
       </header>
     )
 }
